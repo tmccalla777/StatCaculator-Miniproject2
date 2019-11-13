5acf9b0f5c707bbb5d6330c4c28f2aa32b5990f7
bikramsandhu <bss46@njit.edu>
2019-11-13 18:31:08 -0500

    Fix: Added back test_Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,8 +1,27 @@
 from Calculator.Calculator import Calculator
-
+from Statistics.Mean import mean
+from Statistics.Median import median
+from Statistics.Mode import mode
+from Statistics.PopulationStandardDeviation import population_SD
 
 class Statistics(Calculator):
     data = []
 
     def __init__(self):
         super().__init__()
+
+    def mean(self, data):
+        self.data = mean(data)
+        return self.data
+
+    def median(self, data):
+        self.data = median(data)
+        return self.data
+
+    def mode(self, data):
+        self.data = mode(data)
+        return self.data
+
+    def population_SD(self, data):
+        self.data = population_SD(data)
+        return self.data
