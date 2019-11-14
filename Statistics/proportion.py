791d6c511fdfb7de9606827076a1fbeed5028c4a
riya45 <56205260+riya45@users.noreply.github.com>
2019-11-13 21:51:52 -0800

    Update proportion.py
    made minor change in code

--- Statistics/proportion.py
+++ Statistics/proportion.py
@@ -1,1 +1,13 @@
+def proportion(list):
+    try:
+        result = 0
+        num = 0
+        for i in list:
+            if (i % 2) == 0:
+                num = num + 1
 
+            result = num / len(list)
+        return result
+    except Exception as e:
+        print("Exception in Proportion:",e)
+        return 0
