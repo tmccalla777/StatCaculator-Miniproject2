b60eab31431266be110fbb652ec82bbb5faa24ee
riya45 <56205260+riya45@users.noreply.github.com>
2019-11-13 22:08:57 -0800

    Update pvalue.py
    Changes made to the code

--- Statistics/pvalue.py
+++ Statistics/pvalue.py
@@ -6,15 +6,11 @@ def pvalue(a, b, c, d):
     p1 = float(c)
     n = float(b)
     f = float(p - p1)
-    y = float(p1 * (1 - p2))
+    y = float(p1 * (1 - p1))
     y2 = float(y)
     y3 = float(y2 / n)
-    z = float(f / math.sqrt(y2))
+    z = float(f / math.sqrt(y3))
     norm = d
     pval  = 1 - float(norm)
 
     return pval
-
-
-
-
