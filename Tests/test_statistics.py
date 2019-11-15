4f24fcc339717f9484d4449ee39e34ff139883ca
bikramsandhu <bss46@njit.edu>
2019-11-15 00:33:42 -0500

    Feature: Added new .CSV file for mode and changed Mode.py accordingly

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -24,14 +24,13 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.statistics.median(row['Value 1'], row['Value 2'], row['Value 3'], row['Value 4'], row['Value 5'], row['Value 6'], row['Value 7'], row['Value 8'], row['Value 9']), result)
             self.assertEqual(self.statistics.result, result)
 
-    # def test_mode_statistics(self):
-    #     test_data = CsvReader('Tests/Data/unit_test_statistics.csv').data
-    #     result_data = CsvReader('Tests/Data/statistics_results.csv').data
-    #     for row in test_data:
-    #         result = result_data(row['Mode'])
-    #         self.assertEqual(self.statistics.mode(row["TestData"], result))
-    #         self.assertEqual(self.statistics.result, row["Mode"])
-    #
+    def test_mode_statistics(self):
+        test_data = CsvReader('Tests/Data/unit_test_mode.csv').data
+        for row in test_data:
+            result = int(row['Result'])
+            self.assertEqual(self.statistics.mode(row['Value 1'], row['Value 2'], row['Value 3'], row['Value 4'], row['Value 5'], row['Value 6'], row['Value 7'], row['Value 8']), result)
+            self.assertEqual(self.statistics.result, result)
+
     # def test_popSD_statistics(self):
     #     test_data = CsvReader('Tests/Data/unit_test_statistics.csv').data
     #     result_data = CsvReader('Tests/Data/statistics_results.csv').data
