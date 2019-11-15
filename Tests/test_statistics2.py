89e5655c53e44f07919c8ce7eaed8547028847e1
riya45 <56205260+riya45@users.noreply.github.com>
2019-11-15 09:17:20 -0800

    Update test_statistics2.py
    error correction

--- Tests/test_statistics2.py
+++ Tests/test_statistics2.py
@@ -28,24 +28,25 @@ class MyTestCase(unittest.TestCase):
 
 
             self.assertEqual(round(self.extendedstat.proportion_(listx)), round(result_test))
+            
+             # def test_samplemean(self):
+        #
+        #     test_data = csvreader('Tests/csvdata/Array3.csv').data
+        #     test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+        #
+        #     for column in test_result:
+        #         result_test = float(column['samplemean'])
+        #
+        #     listx = []
+        #
+        #     for row in test_data:
+        #         result = float(row['Array'])
+        #         listx.append(result)
+        #
+        #
+        #     self.assertEqual(round(self.extendedstat.samplemean(listx)), round(result_test))
 
-         def test_samplemean(self):
-        
-             test_data = csvreader('Tests/csvdata/Array3.csv').data
-             test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
-        
-             for column in test_result:
-                 result_test = float(column['samplemean'])
-        
-             listx = []
-        
-             for row in test_data:
-                 result = float(row['Array'])
-                listx.append(result)
-       
-       
-            self.assertEqual(round(self.extendedstat.samplemean(listx)), round(result_test))
- def test_pvalue(self):
+         def test_pvalue(self):
 
             test_data = csvreader('Tests/csvdata/UnitArgument.csv').data
             test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
@@ -60,48 +61,34 @@ class MyTestCase(unittest.TestCase):
                 d = float(column['d'])
 
             self.assertAlmostEqual(self.extendedstat.pvalue_(a,b,c,d), result_test)
-            
-            
-             # def test_samplestdev(self):
-        
-             test_data = csvreader('Tests/csvdata/Array3.csv').data
-             test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
-        
-             for column in test_result:
-                 result_test = float(column['samplestdev'])
-        
-             listx = []
-        
-             for row in test_data:
-                 result = float(row['Array'])
-                 listx.append(result)
-        
-             self.assertAlmostEqual(self.extendedstat.samplestdev(listx),result_test)
-        
-        
-         def test_Variance_of_sample_proportion(self):
-        
-             test_data = csvreader('Tests/csvdata/Array3.csv').data
-             test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
-        
-             for column in test_result:
-                 result_test = float(column['Variance_of_sample_proportion'])
-        
-             listx = []
-        
-             for row in test_data:
-                 result = float(row['Array'])
-                 listx.append(result)
-        
-           self.assertAlmostEqual(self.extendedstat.Variance_of_sample_proportion(listx), result_test)
 
-except IndentationError as e :
-    print("Indentation Error in Extended Stat:", e)
-except ImportError as e :
-    print("Import Error in Extended Stat:", e)
-except Exception as e :
-    print("Any Other Kind of Exception:", e)
-except AttributeError as e :
-    print("Attribute Error", e)
-
-        
+        # def test_samplestdev(self):
+        #
+        #     test_data = csvreader('Tests/csvdata/Array3.csv').data
+        #     test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+        #
+        #     for column in test_result:
+        #         result_test = float(column['samplestdev'])
+        #
+        #     listx = []
+        #
+        #     for row in test_data:
+        #         result = float(row['Array'])
+        #         listx.append(result)
+        #
+        #     self.assertAlmostEqual(self.extendedstat.samplestdev(listx),result_test)
+           # def test_Variance_of_sample_proportion(self):
+        #
+        #     test_data = csvreader('Tests/csvdata/Array3.csv').data
+        #     test_result = csvreader('Tests/csvdata/Array3_result2.csv').data
+        #
+        #     for column in test_result:
+        #         result_test = float(column['Variance_of_sample_proportion'])
+        #
+        #     listx = []
+        #
+        #     for row in test_data:
+        #         result = float(row['Array'])
+        #         listx.append(result)
+        #
+        #     self.assertAlmostEqual(self.extendedstat.Variance_of_sample_proportion(listx), result_test)
