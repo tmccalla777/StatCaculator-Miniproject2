9e0720e44ceb6c2dd0e446e30543abcc24eeb26f
tmccalla777 <tnm26@njit.edu>
2019-11-18 16:53:47 -0500

    remove pvalue

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -17,13 +17,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.statistics.mean(row['Value 1'], row['Value 2'], row['Value 3']), result)
             self.assertEqual(self.statistics.result, result)
 
-    def test_p_value(self):
-        test_data = CsvReader('Tests/Data/unit_test_pvalue.csv').data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.statistics.mean(row['Value 1'], row['Value 2'], row['Value 3']), result)
-            self.assertEqual(self.statistics.result, result)
-
 
 
     def test_median_statistics(self):
@@ -40,6 +33,7 @@ class MyTestCase(unittest.TestCase):
             result = float(row['Result'])
             self.assertEqual(self.statistics.zscore(row['Value 1'], row['Value 2'], row['Value 3']), result)
             self.assertEqual(self.statistics.result, result)
+
 
     def test_standardized_score(self):
         test_data = CsvReader("Tests/Data/unit_test_standardized_score.csv").data
